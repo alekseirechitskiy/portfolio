3851bbcc5d9f82cd79a16aa9a0ea30f44c084bf4
slowspirit <slowspirit@gmail.com>
2022-07-17 14:54:15 +0300

    changed links

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-    transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//     publicPath: './',
+//     transpileDependencies: true
+// })
 module.exports = {
+    publicPath: './',
     css: {
         loaderOptions: {
             sass: {
